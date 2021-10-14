eb9b5689c970034c59b1eb4cf142cd0f848caada
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-10-14 13:52:01 +0300

    add: user via devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_104541) do
-
+ActiveRecord::Schema.define(version: 20_211_014_104_541) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,5 +22,4 @@ ActiveRecord::Schema.define(version: 2021_10_14_104541) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
