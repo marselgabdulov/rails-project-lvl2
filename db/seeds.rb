81387f1db68859a5f875c8d2a42ac9aaebfc574a
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-10-16 12:48:35 +0300

    add: seed post_categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+%i[books courses articles].each do |category|
+  PostCategory.create(name: category)
+end
