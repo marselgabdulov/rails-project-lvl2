2a7467ae2dd50dff2efa80fe9f9ef1237de21e31
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-10-16 19:31:47 +0300

    create: post_comments controller, need to add test

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,31 @@
+class PostCommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: %i[create destroy]
+  before_action :check_author, only: %i[destroy]
+
+  def create
+    @post_comment = @post.post_comments.create(user_id: current_user.id, **post_comment_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post_comment = @post.post_comments.find(params[:id])
+    @post_comment.destroy
+    redirect_to post_url(@post)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def check_author
+    redirect_to post_path(@post), notice: 'Действие недопустимо. Вы не автор этого комментария.' if @post.post_comments.find(params[:id]).user != current_user
+  end
+
+  def post_comment_params
+    params.require(:post_comment).permit(:content)
+  end
+
+end
