0f07b39ccc66be2e98faf5c39019a5075059d95d
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-10-16 18:12:30 +0300

    fix: add validations to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :post_category
   belongs_to :user
+  has_many :post_comments
+  validates :title, presence :true
+  validates :body, presence :true
 end
